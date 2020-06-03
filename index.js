ac080025b0155c17410e369d2bcf51435d2a40ec
parkertimmins <parkertimmins@gmail.com>
2020-06-02 23:13:09 -0500

    not working attempt at tait bryan angles and working azimuth

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+goog.require('goog.structs.Heap');
+
+
+
 
 const video = document.querySelector('video');
 const videoSelect = document.querySelector('select#videoSource');
@@ -7,7 +11,96 @@ const ctx1 = canvas1.getContext('2d');
 const canvas2 = document.getElementById('canvas2');
 const ctx2 = canvas2.getContext('2d');
 
-goog.require('goog.structs.PriorityQueue');
+
+
+sensor = new AbsoluteOrientationSensor({frequency: 0.5, referenceFrame: 'device' })
+sensor.addEventListener('reading', e => {
+    
+    
+    //console.log(sensor.quaternion)
+    
+    let angles = quatertionToEulerAngles(sensor.quaternion)
+   
+   
+    let q = toQuatInternal(sensor.quaternion);
+    let q_inv = quatInverse(q)
+
+    let zUnit = [0, 0, 0, -1]
+    
+    let screenVec = quatMult(quatMult(q, zUnit), q_inv) 
+
+
+    let x = screenVec[1]
+    let y = screenVec[2]
+    let z = screenVec[3]
+  
+    const PI = Math.PI
+
+    const thetaPiMax = -Math.atan2(y, x)
+    const theta2PiMax = thetaPiMax < 0 ? 2 * PI + thetaPiMax : thetaPiMax
+    const thetaFromNorth = (theta2PiMax + PI / 2) % (2 * PI)
+    const azimuthRad = thetaFromNorth 
+    
+    
+    let azimuthDeg = azimuthRad * parseFloat(180) / Math.PI
+    console.log(azimuthDeg);
+
+
+    //console.log(screenVec);
+
+
+    let degrees = angles.map(r => r * 180.0 / Math.PI)
+
+
+});
+sensor.start();
+
+
+const atan2 = Math.atan2,
+      asin = Math.asin;
+
+// internal [s, v] - external [v, s]
+function toQuatInternal(q) {
+    return [q[3], q[0], q[1], q[2]]
+}
+
+// https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
+function quatertionToEulerAngles(q) {
+    const a = atan2(
+        2 * (q[0] * q[1] + q[2] * q[3]),
+        1 - 2 * (q[1] ** 2 + q[2] ** 2)
+    );      
+    const b = asin(2 * (q[0] * q[2] - q[3] * q[1]));
+    const c = atan2(
+        2 * (q[0] * q[3] + q[1] * q[2]),
+        1 - 2 * (q[2] ** 2 + q[3] ** 2)
+    );   
+    return [a, b, c]
+}
+
+function quatInverse(q) {
+    const sn = quatSquaredNorm(q)
+    const inv = [q[0], -q[1], -q[2], -q[3]].map(a => a * 1.0 / sn)
+
+    console.log('q', q)
+    console.log('i', inv)
+    return inv
+}
+
+function quatSquaredNorm(q) {
+    return q[0]*q[0] + q[1]*q[1] + q[2]*q[2] + q[3]*q[3]
+
+}
+
+function quatMult(q, r) {
+    return [
+        r[0] * q[0] - r[1] * q[1] - r[2] * q[2] - r[3] * q[3], 
+        r[0] * q[1] + r[1] * q[0] - r[2] * q[3] + r[3] * q[2], 
+        r[0] * q[2] + r[1] * q[3] + r[2] * q[0] - r[3] * q[1], 
+        r[0] * q[3] - r[1] * q[2] + r[2] * q[1] + r[3] * q[0], 
+    ];
+}
+
 
 function run() {
     if (!hasGetUserMedia()) {
@@ -56,8 +149,8 @@ function computeFrame() {
 	let pixels = frame.data.length / 4;
 
 
-    let pq = new goog.structs.PriorityQueue();
-    let numMax = 20;
+    let heap = new goog.structs.Heap();
+    let numMax = 1000;
 	for (let i = 0; i < pixels; i++) {
 		let pixel = i;
 		let r = frame.data[i * 4 + 0];
@@ -66,54 +159,19 @@ function computeFrame() {
 
 		let intensity = r + g + b
 
-        pq.enqueue(intensity, pixel) 
-        
-        if (pq.getCount() > numMax) {
-            pq.dequeue() 
+        heap.insert(intensity, pixel) 
+        if (heap.getCount() > numMax) {
+            heap.remove() 
         }
 	}
 
-    console.log(pq.getKeys())
-    console.log(pq.getValues())
-
-
-    /*    
-	let numMax = 20;
-	let maxPixels =  []
-	for (let i = 0; i < pixels; i++) {
-		let pixel = i;
-		let r = frame.data[i * 4 + 0];
-	  	let g = frame.data[i * 4 + 1];
-	  	let b = frame.data[i * 4 + 2];
-
-		let intensity = r + g + b
-
-		if (maxPixels.length < numMax) {
-			maxPixels.push([intensity, pixel])
-		} else{
-			let lowestMaxIntensity = maxPixels[0][0]	
-			if (intensity > lowestMaxIntensity) {
-				maxPixels[0] = [intensity, pixel]
-			}
-		}
-		
-		maxPixels.sort(function(a, b) {
-  			return a[0] - b[0];
-		});
-	}
-	let rcPairs = maxPixels.map(elem => elem[1])
-			.map(pixel => pixelToRc(pixel, canvas1.width))
-    */
-	
-    let rcPairs = pq.getValues()
-			.map(pixel => pixelToRc(pixel, canvas1.width))
-
-
-
+    let rcPairs = heap.getValues()
+	    .map(pixel => pixelToRc(pixel, canvas1.width))
 	let rCenter = Math.round(avg(rcPairs.map(rc => rc[0])))
 	let cCenter = Math.round(avg(rcPairs.map(rc => rc[1])))
-		console.log(rCenter, cCenter)
-	const square = 10	
+    console.log(rCenter, cCenter)
+    
+    const square = 10	
 	for (let r = rCenter - square; r < rCenter + square; r++) {
 		for (let c = cCenter - square; c < cCenter + square; c++) {
 			let pixel = r * canvas2.width + c
@@ -197,5 +255,5 @@ function hasGetUserMedia() {
 }
 
 window.onload = function() {
-    run();
+    //run();
 }
