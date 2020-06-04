7a33876374a734f0f9023e95eed1eb944d623c61
parkertimmins <parkertimmins@gmail.com>
2020-06-03 22:25:03 -0500

    output lat/long location under sun at a given date time

--- index.js
+++ index.js
@@ -12,94 +12,182 @@ const canvas2 = document.getElementById('canvas2');
 const ctx2 = canvas2.getContext('2d');
 
 
+const atan2 = Math.atan2,
+      asin = Math.asin,
+      atan = Math.atan,
+      sin = Math.sin,
+      cos = Math.cos,
+      PI = Math.PI;
 
-sensor = new AbsoluteOrientationSensor({frequency: 0.5, referenceFrame: 'device' })
+sensor = new AbsoluteOrientationSensor({frequency: 1, referenceFrame: 'device' })
 sensor.addEventListener('reading', e => {
     
-    
-    //console.log(sensor.quaternion)
-    
-    let angles = quatertionToEulerAngles(sensor.quaternion)
    
-   
-    let q = toQuatInternal(sensor.quaternion);
-    let q_inv = quatInverse(q)
+    const deviceOriginVector = [0, 0, -1] 
 
-    let zUnit = [0, 0, 0, -1]
-    
-    let screenVec = quatMult(quatMult(q, zUnit), q_inv) 
+    const quaternion = Quaternions.toInternalQuat(sensor.quaternion)
+    const screenVec = Quaternions.rotate(deviceOriginVector, quaternion) 
 
+    const altitude = toAltitude(screenVec)
+    const azimuth = toAzimuth(screenVec)
 
-    let x = screenVec[1]
-    let y = screenVec[2]
-    let z = screenVec[3]
-  
-    const PI = Math.PI
+    console.log('azimuth', degree(azimuth));
+    console.log('altitude', degree(altitude));
+    console.log('\n')
 
-    const thetaPiMax = -Math.atan2(y, x)
-    const theta2PiMax = thetaPiMax < 0 ? 2 * PI + thetaPiMax : thetaPiMax
-    const thetaFromNorth = (theta2PiMax + PI / 2) % (2 * PI)
-    const azimuthRad = thetaFromNorth 
-    
-    
-    let azimuthDeg = azimuthRad * parseFloat(180) / Math.PI
-    console.log(azimuthDeg);
-
-
-    //console.log(screenVec);
-
-
-    let degrees = angles.map(r => r * 180.0 / Math.PI)
 
 
 });
 sensor.start();
 
 
-const atan2 = Math.atan2,
-      asin = Math.asin;
 
-// internal [s, v] - external [v, s]
-function toQuatInternal(q) {
-    return [q[3], q[0], q[1], q[2]]
+
+
+
+function toAzimuth(vector) {
+    // vector comes from a quaternion ... can throw away scalar 
+    const [_, x, y, z] = vector;
+
+    // [PI, -PI] - positive ccw from east
+    const thetaPiMax = -Math.atan2(y, x)
+    
+    // [0, 2PI] - positive ccw from east
+    const theta2PiMax = thetaPiMax < 0 ? 2 * PI + thetaPiMax : thetaPiMax
+    
+    // [0, 2PI] - positive ccw from north 
+    const thetaFromNorth = (theta2PiMax + PI / 2) % (2 * PI)
+    return thetaFromNorth 
 }
 
-// https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
-function quatertionToEulerAngles(q) {
-    const a = atan2(
-        2 * (q[0] * q[1] + q[2] * q[3]),
-        1 - 2 * (q[1] ** 2 + q[2] ** 2)
-    );      
-    const b = asin(2 * (q[0] * q[2] - q[3] * q[1]));
-    const c = atan2(
-        2 * (q[0] * q[3] + q[1] * q[2]),
-        1 - 2 * (q[2] ** 2 + q[3] ** 2)
-    );   
-    return [a, b, c]
+function toAltitude(vector) {
+    // vector comes from a quaternion ... can throw away scalar 
+    const [_, x, y, z] = vector;
+    
+    const vec_len_on_xy_plane = Math.sqrt(x**2 + y**2)
+    return atan(z/vec_len_on_xy_plane )
 }
 
-function quatInverse(q) {
-    const sn = quatSquaredNorm(q)
-    const inv = [q[0], -q[1], -q[2], -q[3]].map(a => a * 1.0 / sn)
-
-    console.log('q', q)
-    console.log('i', inv)
-    return inv
+function degree(rad) {
+    return rad * 180 / PI
 }
 
-function quatSquaredNorm(q) {
-    return q[0]*q[0] + q[1]*q[1] + q[2]*q[2] + q[3]*q[3]
-
+function rad(degree) {
+    return degree * PI / 180
 }
 
-function quatMult(q, r) {
-    return [
-        r[0] * q[0] - r[1] * q[1] - r[2] * q[2] - r[3] * q[3], 
-        r[0] * q[1] + r[1] * q[0] - r[2] * q[3] + r[3] * q[2], 
-        r[0] * q[2] + r[1] * q[3] + r[2] * q[0] - r[3] * q[1], 
-        r[0] * q[3] - r[1] * q[2] + r[2] * q[1] + r[3] * q[0], 
-    ];
+class Quaternions {
+    // internal [s, v] - external [v, s]
+    static toInternalQuat(q) {
+        return [q[3], q[0], q[1], q[2]]
+    }
+
+    static rotate(vector, quaternion) {
+        const quat_vector = [0].concat(vector);
+        return Quaternions.multiply(
+            Quaternions.multiply(quaternion, quat_vector), 
+            Quaternions.inverse(quaternion)
+        );
+    }
+
+    static squaredNorm(q) {
+        return q[0]*q[0] + q[1]*q[1] + q[2]*q[2] + q[3]*q[3]
+    }
+
+    static multiply(q, r) {
+        return [
+            r[0] * q[0] - r[1] * q[1] - r[2] * q[2] - r[3] * q[3], 
+            r[0] * q[1] + r[1] * q[0] - r[2] * q[3] + r[3] * q[2], 
+            r[0] * q[2] + r[1] * q[3] + r[2] * q[0] - r[3] * q[1], 
+            r[0] * q[3] - r[1] * q[2] + r[2] * q[1] + r[3] * q[0], 
+        ];
+    }
+
+    static inverse(q) {
+        const sn =  Quaternions.squaredNorm(q)
+        return [q[0], -q[1], -q[2], -q[3]].map(a => a * 1.0 / sn)
+    }
 }
+
+
+// https://github.com/mourner/suncalc/blob/master/suncalc.js 
+var dayMs = 1000 * 60 * 60 * 24,
+    J1970 = 2440588,
+    J2000 = 2451545;
+
+function toJulian(date) { 
+    return date.valueOf() / dayMs - 0.5 + J1970; 
+}
+
+function sind(deg) {
+    return sin(rad(deg))
+}
+
+function cosd(deg) {
+    return cos(rad(deg))
+}
+
+function asind(x) {
+    return degree(asin(x))
+}
+function atan2d(x, y) {
+    return degree(atan2(x, y))
+}
+
+
+// https://www.aa.quae.nl/en/reken/zonpositie.html
+// degrees, longitude is [0, 360] west
+function sun_lat_long(date) {
+    const JD = toJulian(date)
+    
+    // mean anomaly
+    const M0 = 357.5291    
+    const M1 = 0.98560028
+    const M = (M0 + M1 * (JD - J2000)) % 360  
+
+    // equation of center
+    const C1 = 1.9148   
+    const C2 = 0.0200  
+    const C3 = 0.0003  
+    const C = C1 * sind(M) + C2 * sind(2 * M) +  C3 * sind(3 * M)
+    
+    // Perihelion and the Obliquity of the Ecliptic
+
+    const ecliptic_long_peri = 102.9373 // perihelion of the earth, relative to the ecliptic and vernal equinox
+    const obliquity = 23.4393 // epsilon
+
+    // The Ecliptical Coordinates
+    
+    // mean ecliptic longitude
+    const L = M + ecliptic_long_peri
+   
+    // ecliptic long of sun 
+    const lambda = L + C + 180
+    const b = 0 // ecliptic lat - divergence of sun from ecliptic is alway 0
+
+    // right ascension and declination
+    const a = atan2d(sind(lambda) * cosd(obliquity), cosd(lambda)) 
+    console.log(sind(lambda))
+    console.log(sind(obliquity))
+    const d = asind(sind(lambda) * sind(obliquity))
+
+    const theta0 = 280.1470 
+    const theta1 = 360.9856235
+    
+    // since we are looking for the place at solar noon, 
+    // and hour angle H = 0 = side real time - right ascension, side real time == ra 
+    // theta(sidereal) = [theta0 + theta1 * (JD - J2000) - lw] mod 360
+    // (A + B) mod C = (A mod C + B mod C) mod C
+   
+    // can treat mod this way? 
+    const longitude = (theta0 + theta1 * (JD - J2000) - a) % 360
+    const latitude = d
+
+    return {
+        longitude, 
+        latitude
+    }
+} 
 
 
 function run() {
@@ -145,19 +233,19 @@ function computeFrame() {
 	ctx1.drawImage(video, 0, 0);
 
 	
-	let frame = ctx1.getImageData(0, 0, canvas1.width, canvas1.height);
-	let pixels = frame.data.length / 4;
+	const frame = ctx1.getImageData(0, 0, canvas1.width, canvas1.height);
+	const pixels = frame.data.length / 4;
 
 
-    let heap = new goog.structs.Heap();
-    let numMax = 1000;
+    const heap = new goog.structs.Heap();
+    const numMax = 1000;
 	for (let i = 0; i < pixels; i++) {
-		let pixel = i;
-		let r = frame.data[i * 4 + 0];
-	  	let g = frame.data[i * 4 + 1];
-	  	let b = frame.data[i * 4 + 2];
+		const pixel = i;
+		const r = frame.data[i * 4 + 0];
+	  	const g = frame.data[i * 4 + 1];
+	  	const b = frame.data[i * 4 + 2];
 
-		let intensity = r + g + b
+		const intensity = r + g + b
 
         heap.insert(intensity, pixel) 
         if (heap.getCount() > numMax) {
@@ -165,17 +253,17 @@ function computeFrame() {
         }
 	}
 
-    let rcPairs = heap.getValues()
+    const rcPairs = heap.getValues()
 	    .map(pixel => pixelToRc(pixel, canvas1.width))
-	let rCenter = Math.round(avg(rcPairs.map(rc => rc[0])))
-	let cCenter = Math.round(avg(rcPairs.map(rc => rc[1])))
+	const rCenter = Math.round(avg(rcPairs.map(rc => rc[0])))
+	const cCenter = Math.round(avg(rcPairs.map(rc => rc[1])))
     console.log(rCenter, cCenter)
     
     const square = 10	
 	for (let r = rCenter - square; r < rCenter + square; r++) {
 		for (let c = cCenter - square; c < cCenter + square; c++) {
-			let pixel = r * canvas2.width + c
-			let i = pixel
+			const pixel = r * canvas2.width + c
+			const i = pixel
 			frame.data[i * 4 + 0] = 255; // r
 			frame.data[i * 4 + 1] = 0; // g
 			frame.data[i * 4 + 2] = 255; // b
@@ -186,14 +274,14 @@ function computeFrame() {
 }
 
 function pixelToRc(pixel, width) {
-	let r = pixel / width
-	let c = pixel % width 
+	const r = pixel / width
+	const c = pixel % width 
 	return [r, c]
 
 }
 
 function avg(nums) {
-	let sum = 0
+	const sum = 0
 	for (let n of nums) {
 		sum += n
 	} 	
@@ -255,5 +343,10 @@ function hasGetUserMedia() {
 }
 
 window.onload = function() {
+    const {latitude, longitude} = sun_lat_long(Date.now())
+
+    const reg_long = longitude <= 180 ? -longitude : 360 - longitude;
+    console.log(latitude + "," + reg_long)
+
     //run();
 }
