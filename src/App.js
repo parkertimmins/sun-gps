0a3f98ae4871f37220b18f58de1ae1c2a3e762fe
parkertimmins <parkertimmins@gmail.com>
2020-07-15 22:35:23 -0500

    Add video to Camera component of react app

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ import 'leaflet/dist/images/marker-shadow.png'
 import { Map, Marker, Popup, TileLayer, GeoJSON } from 'react-leaflet'
 
 
-
 class App extends React.Component {
 
     constructor(props) {
@@ -23,17 +22,22 @@ class App extends React.Component {
             cameraVisible: true
         };
 
-        this.toggleCameraMap = this.toggleCameraMap.bind(this)
+        
+        this.toggleCameraMap = this.toggleCameraMap.bind(this);
 
     }
-        
+
     toggleCameraMap() {
         this.setState({ cameraVisible: !this.state.cameraVisible })
     }
 
+    givePermission() {
+        this.setState({ needUserPermission: false })
+    }
+
     render() {
         if (this.state.needUserPermission) {
-            return <RequestPermsModal givePermission={() => this.setState({ needUserPermission: false })} />
+            return <RequestPermsModal givePermission={() => this.givePermission()} />
         } 
       
         return ( 
@@ -132,6 +136,120 @@ class MapView extends React.Component {
 class CameraView extends React.Component {
     constructor(props) {
         super(props);
+        
+        this.cameras = [];
+        this.selectedCameraIdx = 0;
+    
+        this.addVideoDevicesToCameraList = this.addVideoDevicesToCameraList.bind(this);
+        this.setSelectedStream = this.setSelectedStream.bind(this);
+        this.setCanvasToVideo = this.setCanvasToVideo.bind(this);
+        this.copyVideoToCanvas = this.copyVideoToCanvas.bind(this);
+
+        this.videoRef = React.createRef();
+        this.canvasRef = React.createRef();
+    }
+
+    addVideoDevicesToCameraList(deviceInfos) {
+        for (let deviceInfo of deviceInfos) {
+            if (deviceInfo.kind === 'videoinput') {
+                this.cameras.push(deviceInfo);
+
+                // use this camera if is back camera
+                if (deviceInfo.label && deviceInfo.label.toLowerCase().includes("back")) {
+                    this.selectedCameraIdx = cameras.length - 1;
+                }
+            }
+        }
+    }
+   
+    componentDidMount() {
+        this.initVideo();
+    } 
+
+    initVideo() {
+        /*
+        if (!hasGetUserMedia()) {
+            alert('getUserMedia() is not supported by your browser');
+            return;
+        }
+        */
+
+        // iphone request ask permission before enumerating devices
+        navigator.mediaDevices
+            .getUserMedia({ video: true })
+            .then(() => {
+                navigator.mediaDevices
+                    .enumerateDevices()
+                    .then(this.addVideoDevicesToCameraList)
+                    .then(this.setSelectedStream) // ????
+                    .catch(this.handleError);
+            })
+    }
+
+    setSelectedStream() {
+        this.setVideoToSelectedStream()
+        this.setCanvasToVideo();
+    }
+    
+    setVideoToSelectedStream() {
+        if (window.stream) {
+            window.stream.getTracks().forEach(track => track.stop())
+        }
+
+        const constraints = {
+            video: { deviceId: this.cameras[this.selectedCameraIdx].deviceId  }
+        };
+
+        return navigator.mediaDevices
+            .getUserMedia(constraints)
+            .then(stream => {
+                  window.stream = stream; // make stream available to console
+                  this.videoRef.current.srcObject = stream;
+            })
+    }
+
+    setCanvasToVideo() {
+        this.copyVideoToCanvas();
+        setTimeout(this.setCanvasToVideo, 10);
+    }
+
+    handleError(error) {
+        console.error('Error: ', error);
+    }
+
+    copyVideoToCanvas() {
+        const canvas = this.canvasRef.current;
+        const video = this.videoRef.current;
+        
+        // should probably not do every frame ¯\_(ツ)_/¯
+        this.setCanvasDimensions();
+        
+        if (canvas.width === 0 || canvas.height === 0) {
+            return;
+        }
+    
+        const ctx = canvas.getContext("2d");
+        
+        // first copy frame from video element to canvas
+        ctx.drawImage(video, 0, 0);
+
+        ctx.strokeStyle = "white";
+        ctx.lineWidth = 1 
+        ctx.beginPath();
+        ctx.moveTo(0, canvas.height / 2);
+        ctx.lineTo(canvas.width, canvas.height / 2);
+        
+        ctx.moveTo(canvas.width / 2, 0);
+        ctx.lineTo(canvas.width / 2, canvas.height);
+        ctx.stroke();	
+    }
+
+    // sets logically pixel width of canvas, not size of html element, hence works with width: 100% 
+    setCanvasDimensions() {
+        const canvas = this.canvasRef.current;
+        const video = this.videoRef.current;
+        canvas.width = video.videoWidth
+        canvas.height = video.videoHeight
     }
 
     render() {
@@ -142,8 +260,8 @@ class CameraView extends React.Component {
                 </div>
                 
                 <div id="camera-image" className="fullscreen-pane"> 
-                    <video autoPlay playsInline></video>
-                    <canvas id="canvas1"></canvas>
+                    <video ref={this.videoRef} autoPlay playsInline></video>
+                    <canvas ref={this.canvasRef} id="canvas1"></canvas>
                 </div>
                         
                 <div className="bottom-row control-row"> 
@@ -282,60 +400,6 @@ function copyVideoToCanvas() {
 	ctx.stroke();	
 }
 
-
-function copyVideoFrameWithChanges() {
-	
-	// should probably not do every frame ¯\_(ツ)_/¯
-	setCanvasDimensions();
-
-	if (canvas1.width === 0 || canvas1.height === 0 || canvas2.width === 0 || canvas2.height === 0) {
-		return;
-	}
-	
-	// first copy frame from video element to canvas
-	ctx1.drawImage(video, 0, 0);
-
-	
-	const frame = ctx1.getImageData(0, 0, canvas1.width, canvas1.height);
-	const pixels = frame.data.length / 4;
-
-
-    const heap = new goog.structs.Heap();
-    const numMax = 1000;
-	for (let i = 0; i < pixels; i++) {
-		const pixel = i;
-		const r = frame.data[i * 4 + 0];
-	  	const g = frame.data[i * 4 + 1];
-	  	const b = frame.data[i * 4 + 2];
-
-		const intensity = r + g + b
-
-        heap.insert(intensity, pixel) 
-        if (heap.getCount() > numMax) {
-            heap.remove() 
-        }
-	}
-
-    const rcPairs = heap.getValues()
-	    .map(pixel => pixelToRc(pixel, canvas1.width))
-	const rCenter = Math.round(avg(rcPairs.map(rc => rc[0])))
-	const cCenter = Math.round(avg(rcPairs.map(rc => rc[1])))
-    console.log(rCenter, cCenter)
-    
-    const square = 10	
-	for (let r = rCenter - square; r < rCenter + square; r++) {
-		for (let c = cCenter - square; c < cCenter + square; c++) {
-			const pixel = r * canvas2.width + c
-			const i = pixel
-			frame.data[i * 4 + 0] = 255; // r
-			frame.data[i * 4 + 1] = 0; // g
-			frame.data[i * 4 + 2] = 255; // b
-		}
-	}				
-	
-	ctx2.putImageData(frame, 0, 0);
-}
-
 function pixelToRc(pixel, width) {
 	const r = pixel / width
 	const c = pixel % width 
@@ -409,14 +473,6 @@ function hasGetUserMedia() {
 
 window.onload = function() {
     
-	map = L.map('map', {
-		center: [24.944292, 0.202651],
-		zoom: 2
-	})
-    
-	loadBaseMap();
-	initVideo();
-
 	document.getElementById("find-location-moon").onclick = () => {
 		const { lat, long } = moonComputeLocation(state.altAz, Date.now())
 		const latLong = [lat, toRegLong(long)].map(n => n.toFixed(4))
@@ -430,21 +486,7 @@ window.onload = function() {
 		map.setView(latLong, 5)
 	}
 
-	document.getElementById("show-camera").onclick = () => {
-		document.getElementById("camera-pane").style.display = 'block'
-		document.getElementById("map-pane").style.display = 'none'
-		document.getElementById("show-map").style.display = 'block'
-		document.getElementById("show-camera").style.display = 'none'
-	}
 
-	document.getElementById("show-map").onclick = () => {
-		document.getElementById("camera-pane").style.display = 'none'
-		document.getElementById("map-pane").style.display = 'block'
-		document.getElementById("show-map").style.display = 'none'
-		document.getElementById("show-camera").style.display = 'block'
-		map.invalidateSize()
-	}
-	
     document.getElementById("switch-camera").onclick = () => {
         selectedCameraIdx = (selectedCameraIdx + 1) % cameras.length;
         setSelectedStream();
